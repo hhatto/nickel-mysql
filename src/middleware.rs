d2e2e18fa3c32c0f326c406aca41828738dd7707
Hideo Hattori <hattori-h@klab.com>
2016-08-04 20:45:38 +0900

    fix compile error

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,47 +1,47 @@
 use std::default::Default;
 use std::sync::Arc;
 
-use mysql::conn::MyOpts;
-use mysql::conn::pool::MyPool;
-use mysql::value::from_value;
+use mysql::conn::OptsBuilder;
+use mysql::conn::pool::Pool;
 use nickel::{Request, Response, Middleware, Continue, MiddlewareResult};
 use typemap::Key;
-use plugin::{Pluggable, Extensible};
+use plugin::Extensible;
 
 pub struct MysqlMiddleware {
-    pub pool: Arc<MyPool>,
+    pub pool: Arc<Pool>,
 }
 
 impl MysqlMiddleware {
     pub fn new(db_name: &str, user: &str, pass: &str) -> MysqlMiddleware {
-        let options = MyOpts {
-                user: Some(user.into()),
-                pass: Some(pass.into()),
-                db_name: Some(db_name.into()),
-                ..Default::default()
-        };
-        let pool = MyPool::new(options).unwrap();
-        MysqlMiddleware {
-            pool: Arc::new(pool),
-        }
+        let mut options = OptsBuilder::default();
+        options.user(Some(user))
+            .pass(Some(pass))
+            .db_name(Some(db_name));
+        let pool = Pool::new(options).unwrap();
+        MysqlMiddleware { pool: Arc::new(pool) }
     }
 }
 
-impl Key for MysqlMiddleware { type Value = Arc<MyPool>; }
+impl Key for MysqlMiddleware {
+    type Value = Arc<Pool>;
+}
 
-impl Middleware for MysqlMiddleware {
-    fn invoke<'res>(&self, request: &mut Request, response: Response<'res>) -> MiddlewareResult<'res> {
+impl<D> Middleware<D> for MysqlMiddleware {
+    fn invoke<'mw, 'conn>(&self,
+                          request: &mut Request<'mw, 'conn, D>,
+                          response: Response<'mw, D>)
+                          -> MiddlewareResult<'mw, D> {
         request.extensions_mut().insert::<MysqlMiddleware>(self.pool.clone());
         Ok(Continue(response))
-    }    
+    }
 }
 
 pub trait MysqlRequestExtensions {
-    fn db_connection(&self) -> Arc<MyPool>;    
+    fn db_connection(&self) -> Arc<Pool>;
 }
 
-impl<'a, 'b, 'c> MysqlRequestExtensions for Request<'a, 'b, 'c> {
-    fn db_connection(&self) -> Arc<MyPool> {
+impl<'a, 'b, D> MysqlRequestExtensions for Request<'a, 'b, D> {
+    fn db_connection(&self) -> Arc<Pool> {
         self.extensions().get::<MysqlMiddleware>().unwrap().clone()
-    }    
+    }
 }
